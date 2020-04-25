2f916274553826ab241d54c6bf97c6482e4cd5ad
rhinoc <rhinoc@outlook.com>
2020-04-25 23:57:00 +0800

    Updates

---
+++ precache-manifest.d618c6995d46c28708cce8130359f993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e751300bc76e3b2b6855446887966c6",
+    "url": "/typing-test/index.html"
+  },
+  {
+    "revision": "b091070b97c22b3be536",
+    "url": "/typing-test/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "255531eac4fd48aa39a1",
+    "url": "/typing-test/static/css/main.66767645.chunk.css"
+  },
+  {
+    "revision": "b091070b97c22b3be536",
+    "url": "/typing-test/static/js/2.5f720917.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/typing-test/static/js/2.5f720917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255531eac4fd48aa39a1",
+    "url": "/typing-test/static/js/main.be4eb130.chunk.js"
+  },
+  {
+    "revision": "2249296254e5847e574f",
+    "url": "/typing-test/static/js/runtime-main.804c91d3.js"
+  }
+]);
